3019d1f0ed4e0ee7c8ca01fcdb5fbffde1baa746
aubit <aubit@naver.com>
2025-10-10 03:45:52 +0900

    1

--- Terraform/AWS/envs/security.tf
+++ Terraform/AWS/envs/security.tf
@@ -27,5 +27,19 @@ resource "aws_security_group" "sg_alb" {
     }
 
     tags = { Name = "sg-alb" }
+
+}
+
+resource "aws_security_group" "eks_cluster" {
+    name = "sg_eks_cluster"
+    vpc_id = aws_vpc.main.id
     
+    egress {
+        from_port = 10250
+        to_port = 10250
+        protocol = "tcp"
+        security_groups = [aws_security_group.sg_eks_node.id]
+        description = "control plane > nodes"
+
+    }
 }
