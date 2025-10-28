49388a4c6de350d53f3085ca4789835acd9048bd
aubit <aubit@naver.com>
2025-10-28 21:12:50 +0900

    1

--- Terraform/AWS/envs/variables.tf
+++ Terraform/AWS/envs/variables.tf
@@ -44,6 +44,16 @@ variable "vpc_1_cidr" {
 
 
 # Subnet
+variable "public_subnets" {
+  description = "public subnet CIDR list"
+  type        = list(string)
+  default     = [
+    "10.0.10.0/24", # ap-northeast-2a
+    "10.0.11.0/24", # ap-northeast-2b
+    "10.0.12.0/24", # ap-northeast-2c
+  ]
+  
+}
 variable "private_subnets" {
   description = "private subnet CIDR list"
   type        = list(string)
@@ -53,15 +63,7 @@ variable "private_subnets" {
     "10.0.3.0/24", # ap-northeast-2c
   ]
 }
-variable "public_subnets" {
-  description = "public subnet CIDR list"
-  type        = list(string)
-  default     = [
-    "10.0.10.0/24", # ap-northeast-2a
-    "10.0.11.0/24", # ap-northeast-2b
-    "10.0.12.0/24", # ap-northeast-2c
-  ]
-}
+
 
 
 
--- Terraform/AWS/envs/vpc.tf
+++ Terraform/AWS/envs/vpc.tf
@@ -1,3 +1,17 @@
-resource "aws_vpc" "main" {
+resource "aws_subnet" "main_public_subnet" {
+  for_each = {
+    "1a" = var.public_subnets[0]
+    "1b" = var.public_subnets[1]
+    "1c" = var.public_subnets[2]
+  }
   
+  vpc_id               = aws_vpc_.main.id
+  cidr_block           = each.value
+  availability_zone    = var.region
+
+  tags = {
+    name = "main_public_subnet"
+  }
 }
+
+
