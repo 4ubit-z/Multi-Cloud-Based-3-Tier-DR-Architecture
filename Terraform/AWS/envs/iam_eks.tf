ea7c448bdfe63dc3bd1a6a2c6027f1a52dc24da1
aubit <aubit@naver.com>
2025-11-01 21:00:23 +0900

    1

--- Terraform/AWS/envs/iam_eks.tf
+++ Terraform/AWS/envs/iam_eks.tf
@@ -28,6 +28,28 @@ resource "aws_iam_role_policy_attachment" "eks_vpc_controller" {
 
 #eks_node_group role
 resource "aws_iam_role" "eks_node_role" {
-  name = var.eks_cluster_1
+  name = "eks_cluster1-node-group"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action   = "sts:AssumeRole"
+    }]
+  })
+}
+
+#eks_node policy
+resource "aws_iam_role_policy_attachment" "node_worker" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+resource "aws_iam_role_policy_attachment" "node_cni" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+resource "aws_iam_role_policy_attachment" "node_ecr_ro" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
