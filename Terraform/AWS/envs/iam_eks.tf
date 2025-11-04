820cb0f39cf12599238af091ee9741774f525dad
aubit <aubit@naver.com>
2025-11-04 19:37:26 +0900

    1

--- Terraform/AWS/envs/iam_eks.tf
+++ Terraform/AWS/envs/iam_eks.tf
@@ -4,11 +4,11 @@ resource "aws_iam_role" "eks_cluster_role" {
   path = "/eks/"
   
   assume_role_policy = jsonencode({
-    version = "2012-10-17",
+    Version = "2012-10-17",
     Statement = [{
         Effect = "Allow",
         Principal = { Service = "eks.amazonaws.com" },
-        Action = "sts:AssumRole"
+        Action   = "sts:AssumeRole"   
     }]
   })
 }
