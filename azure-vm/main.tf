88712081f3d75df3cfe4211b7de8b939e847fa47
dhs <dohyeonsuk12@gmai.com>
2025-11-05 21:00:54 +0900

    azure-vm setting

---
+++ azure-vm/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "6692c162-4611-41d5-88d5-e8732f36579b"  # ✅ 네가 로그인한 Azure 구독 ID
+}
+
+# 1. 리소스 그룹 생성
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-rg"
+  location = "Korea Central"
+}
+
+# 2. 가상 네트워크 + 서브넷
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "terraform-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 3. 퍼블릭 IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "terraform-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# 4. 네트워크 인터페이스
+resource "azurerm_network_interface" "nic" {
+  name                = "terraform-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# 5. 가상 머신
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "terraform-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")  # SSH 키 경로 (Windows는 C:\Users\사용자\.ssh\id_rsa.pub)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
