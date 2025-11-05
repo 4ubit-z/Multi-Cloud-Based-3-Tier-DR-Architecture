e70dce062c2eb140fea4a18b7c40c745ca378e99
dhs <dohyeonsuk12@gmail.com>
2025-11-05 21:47:29 +0900

    setting

---
+++ terraform-azure-vm/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.51.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "6692c162-4611-41d5-88d5-e8732f36579b"
+}
+
+# 1️⃣ 리소스 그룹 생성
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-rg-demo"
+  location = "Korea Central"
+}
+
+# 2️⃣ 가상 네트워크 생성
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# 3️⃣ 서브넷 생성
+resource "azurerm_subnet" "subnet" {
+  name                 = "tf-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4️⃣ 공용 IP 생성
+resource "azurerm_public_ip" "public_ip" {
+  name                = "tf-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# 5️⃣ 네트워크 인터페이스 생성
+resource "azurerm_network_interface" "nic" {
+  name                = "tf-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "tf-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# 6️⃣ 가상 머신 생성
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "tf-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_password = "Password1234!"  # 테스트용. 실제로는 Key Vault나 변수로 관리하는 것이 안전함.
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
